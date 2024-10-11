e78664dcd75db73376b0ee3523bb4bb180cde533
Dhayalini P. <UT02010@unicomtic.com>
2024-10-11 13:22:33 +0530

    final commit

---
+++ FitnessProgramManagementSystem_V2/FitnessProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem_V2
+{
+    internal class FitnessProgram
+    {
+
+        public string FitnessProgramId { get; set; }
+        public string Title{ get; set; }
+        public string Duration { get; set; }
+        public decimal Price { get; set; }
+
+        public FitnessProgram(string fitnessProgramId, string title, string duration, decimal price)
+        {
+            FitnessProgramId = fitnessProgramId;
+            Title = title;
+            Duration = duration;
+            Price = price;
+        }
+
+        //public override string ToString(string fitnessProgramId, string title, string duration, decimal price) 
+        //{
+
+        //    return $"fitnessProgramId: {FitnessProgramId}, Title: {Title}, Duration: {Duration}, Price: {Price}";
+        //}
+    }
+}
---
+++ FitnessProgramManagementSystem_V2/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem_V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FitnessProgramRepository repository = new FitnessProgramRepository();
+
+            bool exit=false;
+            while (!exit) 
+            {
+                Console.Clear();
+                Console.WriteLine("FitnessProgram Management System:");
+                Console.WriteLine("1. Add a FitnessProgram");
+                Console.WriteLine("2. View All FitnessPrograms");
+                Console.WriteLine("3. Update a FitnessProgram");
+                Console.WriteLine("4. Delete a FitnessProgram");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Choose an option: ");
+                string option= Console.ReadLine();
+            }
+
+            switch (repository)
+            {
+                case "1":
+                    Console.WriteLine("Add a FitnessProgram");
+                    CreateFitnessProgram(repository);
+                    break;
+                case "2":
+                    Console.WriteLine(" View All FitnessProgram");
+                    ReadFitnessPrograms(repository);
+                    break;
+                case "3":
+                    Console.WriteLine("Update a FitnessProgram");
+                    UpdateFitnessProgram(repository);
+                    break;
+                case "4":
+                    Console.WriteLine("Delete a FitnessProgram\"");
+                    DeleteFitnessProgram(repository);
+                    break;
+                case "5":
+                    Console.WriteLine(" Exit");
+                   exit = true;
+                    break;
+            }
+            if(!exit){
+                Console.WriteLine("Choose an option: 1");
+
+            }
+
+        }
+
+        private static void CreateFitnessProgram(FitnessProgramRepository repository)
+        {
+            Console.WriteLine("Enter FitnessProgram ID");
+            string ID = Console.ReadLine();
+
+            Console.WriteLine("Enter FitnessProgram Title");
+            string Title = Console.ReadLine();
+
+            Console.WriteLine("Enter FitnessProgram Duration");
+            string Duration = Console.ReadLine();
+
+            Console.WriteLine("Enter FitnessProgram Price");
+            string Price = Console.ReadLine();
+
+            Console.WriteLine("FitnessProgram added successfully.");
+
+            //manager.CreateFitnessProgram(ID, Title, Duration, Price);
+        }
+        private static void UpdateFitnessProgram(FitnessProgramRepository repository)
+        {
+            Console.WriteLine("Enter FitnessProgram ID to update");
+            string ID = Console.ReadLine();
+
+            Console.WriteLine("Enter FitnessProgram Title");
+            string Title = Console.ReadLine();
+
+            Console.WriteLine("Enter FitnessProgram Duration");
+            string Duration = Console.ReadLine();
+
+            Console.WriteLine("Enter FitnessProgram Price");
+            string Price = Console.ReadLine();
+
+            Console.WriteLine("FitnessProgram Updated successfully.");
+
+            repository.UpdateFitnessProgram(ID, Title, Duration, Price);
+        }
+
+        private static void DeleteFitnessProgram(FitnessProgramRepository repository)
+        {
+            Console.WriteLine("Enter FitnessProgram ID to deleted");
+            string ID = Console.ReadLine();
+
+   
+
+            Console.WriteLine("FitnessProgram deleted successfully.");
+
+            repository.DeleteFitnessProgram(ID);
+        }
+    }
+
+        }
+    
+
